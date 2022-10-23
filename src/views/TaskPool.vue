36d7d2351046ab76f1a044f04db2615cfe039a18
leri <214zzl995@gmail.com>
2022-10-23 22:24:14 +0800

    cun

--- src/views/TaskPool.vue
+++ src/views/TaskPool.vue
@@ -5,7 +5,25 @@
     </el-header>
     <el-container>
       <el-aside class="TaskPoolAside" width="25%">
-        <el-input v-model="searchContent"></el-input>
+        <!-- 当前的几种查找的方案都不合适 暂定方案为添加查询按钮 点击查询按钮 或键盘快捷键 c+f 时 悬浮一个和edge 差不多的 搜索框 这个搜索框的位置需要斟酌一下 如何符合一个人的操作 向下选择和 向上选择是需要的 需要定位到位置并滚动过去  -->
+        <!-- 还有就是 对于下拉的设计 应该会做二级菜单 二级菜单的颜色选择是灰色  缩进与下面的选择器相同 缩进需要斟酌一下 -->
+        <!-- 当前需要对不同的情况有默认的处理 比如打开时 树是什么状态的  -->
+        <!-- 按钮的添加 需要全部缩回去的按钮 全部打开的按钮 搜索按钮 -->
+        <!-- 动画 他们值得动画 虽然动画我还不知道怎么做 但是应该要有 -->
+        <!-- <el-input v-model="searchContent"></el-input> -->
+        <div class="operate">
+          <div
+            class="operate-item"
+            @mousemove="operateHover.search = true"
+            @mouseleave="operateHover.search = false"
+          >
+            <search
+              theme="outline"
+              size="24"
+              :fill="operateHover.search ? '#909399' : '#333'"
+            />
+          </div>
+        </div>
       </el-aside>
       <el-main class="TaskPoolMain"> Main </el-main>
     </el-container>
@@ -13,8 +31,14 @@
 </template>
 
 <script lang="ts" setup>
-const searchContent = ref("");
+import { Search } from "@icon-park/vue-next";
 
+const operateHover = ref({
+  search: false,
+  fold: false,
+  expand: false,
+});
+const searchContent = ref("");
 
 //搜索内容防抖 在修改搜索内容时 如果前面还在执行就先清除后继续
 let SearchTimeOut: any;
@@ -25,21 +49,33 @@ watch(searchContent, (newValue, oldValue) => {
     console.log(newValue);
   }, 2000);
 });
-
-
 </script>
 
 <style lang="less" scoped>
 .header {
+  background: #f0f2f5;
   padding: 0;
 }
 .TaskPool {
-  background: rgb(154, 150, 150);
+  background: #fafafa;
   width: 100vw;
   height: 100vh;
 }
 .TaskPoolAside {
-  background: white;
   height: 100%;
+  box-shadow: 0px 12px 32px 4px rgba(0, 0, 0, 0.04),
+    0px 8px 20px rgba(0, 0, 0, 0.08);
+}
+
+.operate {
+  padding-right: 0.5rem;
+  padding-top: 0.5rem;
+  display: flex;
+  flex-direction: row-reverse;
+  cursor: pointer;
+}
+
+.operate-item:hover {
+  cursor: pointer;
 }
 </style>
